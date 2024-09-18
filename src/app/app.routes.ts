f383a5eb9d0787facdb2e795f8741ea9eea9ea46
Gust4v0M <gustavomorgado6@gmail.com>
2024-09-18 19:41:38 -0300

    New project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { WealcomePageComponent } from './wealcome-page/wealcome-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'', component: WealcomePageComponent
+    }
+];
---
+++ src/app/wealcome-page/wealcome-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wealcome-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './wealcome-page.component.html',
+  styleUrl: './wealcome-page.component.css'
+})
+export class WealcomePageComponent {
+
+}
