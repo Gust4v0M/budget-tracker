b41dd6d2ead65f3b7d7e4d5dd2dc380cd2ad1b57
Gust4v0M <gustavomorgado6@gmail.com>
2024-09-23 19:19:42 -0300

    Function of select period done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  selectedPeriod: string = 'day'; 
 
+  chooseColor(period: string) {
+    this.selectedPeriod = period;
+  }
+
+  getBackgroundColor(period: string): string {
+    return this.selectedPeriod === period ? '#00d09e' : '#dff6e2';
+  }
 }
