49ad9147c7ee200b5b2140cf22b7e1c95c83db35
Gust4v0M <gustavomorgado6@gmail.com>
2024-12-30 22:36:26 -0300

    Alterações feitas para rodar a lista funcionando

--- budget-tracker-angular/src/app/home/home.component.ts
+++ budget-tracker-angular/src/app/home/home.component.ts
@@ -1,14 +1,13 @@
+
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+
 import { ServicesService } from '../services/services.service';
-import { Users } from '../models/users';
-import { map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ CommonModule, AsyncPipe],
+  imports: [ CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
@@ -20,8 +19,8 @@ export class HomeComponent  implements OnInit{
 
   ngOnInit(): void {
       this.service.list().subscribe((res: any) =>
-        this.result$ = res[0].transactions,
-      tap(res => console.log(res)))
+        this.result$ = res.flatMap((user: any) => user.transactions)
+    );
   }
 
 
@@ -33,7 +32,5 @@ export class HomeComponent  implements OnInit{
   getBackgroundColor(period: string): string {
     return this.selectedPeriod === period ? '#00d09e' : '#dff6e2';
   }
-
-
 
 }
